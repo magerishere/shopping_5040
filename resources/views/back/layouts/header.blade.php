519bf1b73881bfd2a083ed8621a42293786cb103
magerishere <immagerishere@gmail.com>
2023-05-23 19:23:00 +0330

    Change Menu Text To English

--- resources/views/back/layouts/header.blade.php
+++ resources/views/back/layouts/header.blade.php
@@ -10,7 +10,7 @@
                     <li>
                         <a href="#" class="nav-link text-white">
                             <i class="bi bi-house mx-auto mb-1"></i>
-                            خانه
+                            Home
                         </a>
                     </li>
                 </ul>
