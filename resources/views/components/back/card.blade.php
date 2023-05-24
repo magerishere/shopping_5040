2407e2a96e7305ec3be390dc16c2743667e17da7
magerishere <immagerishere@gmail.com>
2023-05-24 07:09:32 +0330

    change typo session

--- resources/views/components/back/card.blade.php
+++ resources/views/components/back/card.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-body">
-        @session('session_message')
+        @session('session_alert')
         <div class="alert alert-{{session()->get('session_alert_type')}}">
             {{session()->get('session_alert')}}
         </div>
